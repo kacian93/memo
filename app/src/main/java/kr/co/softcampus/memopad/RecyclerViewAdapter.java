2223301400dda6f0f549ee4cc16fbd89b9a473c9
kacian <kacian93@gmail.copm>
2019-10-30 20:21:52 +0900

    191028 1.メモの内容が長くなると、メモの枠により日時の枠が崩れてしまうのを直しました. 2.メモが長くなる時要約できた表紙するメモを1行にしました。

--- app/src/main/java/kr/co/softcampus/memopad/RecyclerViewAdapter.java
+++ app/src/main/java/kr/co/softcampus/memopad/RecyclerViewAdapter.java
@@ -33,8 +33,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         }
         void onBind(Memo data) {
             if(data.getMemoContent().length()>30) {
-                text1.setTextSize(14);
-                text1.setText(data.getMemoContent().substring(0, 30)+"…");
+                text1.setText(data.getMemoContent().substring(0, 30));
 
             }else{
                 text1.setText(data.getMemoContent());
